27ca3639fc6a2c2e809cc53984cca2217024ff2a
PhyzXeno <630172407@qq.com>
2018-10-18 15:12:34 +0800

    add my_ip.sh

---
+++ src/my_ip.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+import "log"
+import "net/http"
+import "io/ioutil"
+import "regexp"
+
+
+func main() {
+	url := "http://ip4.me"
+	var str string
+	var re = regexp.MustCompile(`\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b`)
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	str = string(body)
+	fmt.Println("Current ip is:",re.FindString(str))
+}
