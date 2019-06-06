04aad3b9c51f508e71398edb380953a4e5f3cb97
PhyzXeno <630172407@qq.com>
2019-06-06 12:32:23 +0800

    go build

--- src/github.com/lin/main.go
+++ src/github.com/lin/main.go
@@ -2,11 +2,19 @@ package main
 import (
 	"fmt"
 	"github.com/lin/my"
+	"net/http"
+	"io/ioutil"
 )
 
 func main(){
 	fmt.Printf("hello\n")
 	my.Test()
-	my.Myip()
-	fmt.Scanln()
+	// my.Myip()
+	// fmt.Scanln()
+	url := "http://www.baidu.com"
+	res, _ := http.Get(url)
+	// fmt.Printf("%d, %s\n",res.StatusCode,res.Proto)
+	body, _ := ioutil.ReadAll(res.Body)
+	defer res.Body.Close()
+	fmt.Printf("%s\n", body)
 }
