52468b394cc41a525a6148d6a132554a32b2e83e
PhyzXeno <630172407@qq.com>
2019-06-10 21:55:57 +0800

    openvpn

--- src/github.com/lin/cmd/main.go
+++ src/github.com/lin/cmd/main.go
@@ -1,14 +1,16 @@
 package main
 
-import "net"
+// import "net"
 import "fmt"
+import "github.com/lin/mysocks"
 
 func main(){
-	listenAddr := "127.0.1.1:8080"
-	structListenAddr, err := net.ResolveTCPAddr("tcp", listenAddr)
-	if err != nil {
-		return
-	}
-	fmt.Println(structListenAddr.IP[0:])
-	fmt.Println(structListenAddr.Port)
+	// listenAddr := "127.0.1.1:8080"
+	// structListenAddr, err := net.ResolveTCPAddr("tcp", listenAddr)
+	// if err != nil {
+	// 	return
+	// }
+	// fmt.Println(structListenAddr.IP[0:])
+	// fmt.Println(structListenAddr.Port)
+	fmt.Println(lightsocks.RandPassword())
 }
